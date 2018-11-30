b904579d3a5ca39bae82e1df2a31bf47e0427477
huangxiaolei579 <836287453@qq.com>
2018-11-30 18:20:32 +0800

    updata :  net name

--- app/config.js
+++ app/config.js
@@ -1,10 +1,10 @@
 export default {
-  mainHttpEndpoint: 'https://apiserver.eoscannon.io',
+  mainHttpEndpoint: 'https://mainnet.eoscannon.io',
   mainChainId:
     'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
 
-  testHttpEndpoint: 'https://tool.eoscannon.io/jungle',
-  testEndpoint: 'tool.eoscannon.io/jungle',
+  testHttpEndpoint: 'https://worbli.eoscannon.io',
+  testEndpoint: 'worbli.eoscannon.io',
   testChainId:
-    '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca'
+    '73647cde120091e0a4b85bced2f3cfdb3041e266cbbe95cee59b73235a1b3b6f'
 }
